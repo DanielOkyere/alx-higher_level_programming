d4ee4cc17edce4e1b1e3fdb0101977d743ad724c
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-06-30 15:42:28 +0000

    Who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+if __name__ == "__main__":
+    l = dir(hidden_4)
+    for i in range(len(l)):
+        if(l[i][0] != '_'):
+            print("{}".format(l[i]))
