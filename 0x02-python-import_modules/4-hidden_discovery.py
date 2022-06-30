c7576deb84af0ee9e589a8bd528960288aaff0f1
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-06-30 16:19:51 +0000

    Everything imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,7 @@ import hidden_4
 
 if __name__ == "__main__":
     l = dir(hidden_4)
+    avoid = "__"
     for i in range(len(l)):
-        if(l[i][0] != '_'):
-            print("{}".format(l[i]))
+        if avoid not in l[i]:
+            print(l[i])
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from variable_load_5 import a
+
 if __name__ == "__main__":
-    b = a
-    print("{:d}".format(b))
+    from variable_load_5 import a
+    print(a)
