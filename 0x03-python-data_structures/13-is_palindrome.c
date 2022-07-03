04edc9065a1088a3041acaa427aec0168b861446
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-07-03 06:37:36 +0000

    fixed pointer issues for palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -16,7 +16,7 @@ int is_palindrome(listint_t **head)
 	listint_t *midnode = NULL;
 	int res = 1;
 
-	if (*head != NULL)
+	if (*head != NULL && (*head)->next != NULL)
 	{
 		while (fptr != NULL && fptr->next != NULL)
 		{
