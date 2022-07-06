d88d21dddd1932e13a04902ee2d3de062526b929
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2022-07-06 22:13:58 +0000

    Number of keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     count = 0
-    for key,value in a_dictionary.items():
+    for key, value in a_dictionary.items():
         count += 1
 
     return count
