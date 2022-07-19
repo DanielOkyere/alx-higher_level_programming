5a5215504076ed32416c57c27c1392abcb0f6f0b
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-07-19 14:11:06 +0000

    2-square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A square class definition"""
+
+
+class Square:
+    """ This class defines what a square class
+    is. 
+    Attributes:
+        __size (int): size of a square
+    """
+
+    __size = 0
+    
+    def __init__(self, size=0):
+        """Args:
+                size (int): Interger and should
+                be greater than 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >=0")
+            else:
+                self.__size = size
+        
