603e63d196d3d3b48412130e61d4b91460a2e1fc
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-07-19 16:35:53 +0000

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,8 +58,11 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for i in range(self.__size):
-            print("".join(["#" for j in range(self.__size)]))
+        for i in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            print("".join([" " for k in range(self.__position[0])]), end="")
+            print("".join(["#" for l in range(self.__size)]))
 
     @property
     def position(self):
