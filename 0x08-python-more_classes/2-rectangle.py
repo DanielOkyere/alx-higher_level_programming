58dd0f0f5113a55f3ead00fa0de09ba03885e99e
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-07-25 19:56:40 +0000

    2-rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""A rectangle class"""
+
+
+class Rectangle:
+    """This defines a class for the rectangle class
+    This class has a private attribute width and height
+    The values of width and height should only be integers.
+    a `TypeError` exception would be raise if any is not an int
+    """
+    def __init__(self, width=0, height=0):
+        """The init initializes with height at 0, and width at 0
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the area of the triangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
