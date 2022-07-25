5ae4ba7e681b29ee24d9c8c63766e2d5751e2dcf
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-07-25 20:55:36 +0000

    fining 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -68,6 +68,7 @@ class Rectangle:
         if self.__width != 0 and self.__height != 0:
             string += "\n".join(str(self.print_symbol) * self.__width
                                 for j in range(self.__height))
+        return string
 
     def __repr__(self):
         """ represent self for eval"""
