f22e034b1f18998f9f85b405584d9a1f4bfb0653
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-07-25 20:14:47 +0000

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -56,8 +56,9 @@ class Rectangle:
         """Returns printable string representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width
-                                for j in range(self.__height))
+            return string
+        string += "\n".join("#" * self.__width
+                            for j in range(self.__height))
         return string
 
     def __repr__(self):
