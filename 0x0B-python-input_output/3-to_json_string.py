1b757a664c9816a4fa01fb0983102e7e04227130
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2022-08-02 21:58:04 +0000

    I/O 4

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""To Json String"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string"""
+    return json.dumps(my_obj)
