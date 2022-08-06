2bd5953916e43154fb62e6b024ca870ccc34f801
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2022-08-06 08:34:54 +0000

    9-main.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,6 +13,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initializer for Square"""
         super().__init__(size, size, x, y, id)
+        self.__size = size
     
     @property
     def size(self) -> int:
