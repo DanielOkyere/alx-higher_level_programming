dae4050db8c7bb49bd75d7269a9a6b54ed88eb81
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-09-07 21:58:59 +0000

    8 fixes

--- 0x0E-SQL_more_queries/8-cities_of_carlifonia_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_carlifonia_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all cities in the database
 SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM states WHERE name = "California")
 ORDER BY cities.id ASC;
