3d27a8e87980ab17e6aa2732b1624a3473065aa4
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-09-07 22:50:04 +0000

    fixing genres for 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,4 @@ FROM tv_show_genres INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id AND tv_shows.title = 'Dexter'
-OEDER BY name ASC;
+ORDER BY name ASC;
