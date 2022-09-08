72600bf0d49c2b32657a0354b2914a1505333a4a
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-09-08 22:34:53 +0000

    fixes for 19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 INNER JOIN tv_show_ratings ON tv_show.id = tv_show_ratings.show_id
-GROUP BY tv_show_ratings.show_id
+GROUP BY tv_show_ratings.id
 ORDER BY rating DESC;
