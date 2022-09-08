826dd193b3808810b05e9c08ba6ed5514caef717
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-09-08 22:27:43 +0000

    100 fixes

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,4 +7,5 @@ WHERE g.name NOT IN (
       INNER JOIN tv_show_genres m ON g.id = m.genre_id
       INNER JOIN tv_show s ON m.show_id = s.id
       WHERE s.title = 'Dexter'
-) ORDER BY g.name ASC;
+)
+ORDER BY g.name ASC;
