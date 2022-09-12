d7f624ef81df88a782d3a226cfbea17d05696869
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-09-12 07:05:27 +0000

    fix: 100-not_my_genres

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,14 @@
--- show `not in my genres`
-SELECT tv_genres.name
-FROM tv_genres
-WHERE tv_genres.id NOT IN (
-      SELECT tv_genres.id
-      FROM tv_genres
-      INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-      INNER JOIN tv_show ON tv_show_genres.show_id = tv_show.id
-      WHERE tv_shows.title = "Dexter"
+-- Use 'hbtn_0d_tvshows' to list all genres not linked to show 'Dexter'
+-- 'tv_shows' table containes only one record while title = Dexter
+-- Each record should display tv_genres.name
+-- Results must be sorted ascending order by genre name
+SELECT g.name
+FROM tv_genres g
+WHERE g.name NOT IN (
+      SELECT g.name
+      FROM tv_genres g
+      INNER JOIN tv_show_genres m ON g.id = m.genre_id
+      INNER JOIN tv_show s ON m.show_id = s.id
+      WHERE s.title = 'Dexter'
 )
-ORDER BY tv_genres.name;
+ORDER BY g.name;
