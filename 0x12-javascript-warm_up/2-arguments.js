0bfe349dec621597599c3f9394f79876772c7455
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-03 19:53:39 +0000

    fix:2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
  * This program prints a message depending on the number of
  * arguments passed
  */
-if (process.argv.length === 2) {
+if (process.argv.length <= 2) {
   console.log('No argument');
 } else {
   console.log('Arguments found');
