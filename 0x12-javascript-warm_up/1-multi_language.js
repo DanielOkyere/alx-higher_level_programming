01f3f6f5fc2c3153a0807669e3585e2b35d5e30d
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-03 19:16:03 +0000

    1-multi_language done

---
+++ 0x12-javascript-warm_up/1-multi_language.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * This script prints out 3 lines
+ * 1. C is fun
+ * 2. Python is cool
+ * 3. JavaScript is amazing
+ */
+let myVar = 'C is fun';
+console.log(myVar);
+myVar = 'Python is cool';
+console.log(myVar);
+myVar = 'JavaScript is amazing';
+console.log(myVar);
