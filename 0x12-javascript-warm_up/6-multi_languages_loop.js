5978d81128a1123db7ea9ab83a83f5d4304c49bf
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-03 20:34:07 +0000

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+ * Script prints 3 lines using an array
+ */
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
