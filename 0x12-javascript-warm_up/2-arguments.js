71dde4b4c177537758abc2ceb29750afe7142433
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-03 20:00:08 +0000

    fix:2-arguments.js edge case

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,6 +5,8 @@
  */
 if (process.argv.length <= 2 || !process.argv) {
   console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
