6063829d1f41611ac2e0971192b2ccf39c23fa41
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-04 17:06:56 +0000

    6-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,8 +8,8 @@ class Square extends require('./5-square.js') {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.size; i++) {
-        console.log(c.repeat(this.size));
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
       }
     }
   }
