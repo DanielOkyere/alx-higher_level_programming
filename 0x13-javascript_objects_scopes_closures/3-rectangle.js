755af300d1a31b5c6497b831ff3be9114ff795f8
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-04 16:46:13 +0000

    fix: documentation for 3-rectangel

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,6 +5,10 @@
  * If w or h is 0 an empty object is created
  */
 class Rectangle {
+  /**
+   * Constructor takes 2 args; w and h.
+   * Creates an empty object if w or h is not 0 or positive
+   */
   constructor (h, w) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -12,6 +16,9 @@ class Rectangle {
     }
   }
 
+  /**
+   * Method prints Rectangle with character 'X'/
+   */
   print () {
     for (let i = 0; i < this.width; i++) {
       console.log('X'.repeat(this.height));
