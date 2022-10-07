b183847dbd0c2049ddfbd4f0983eb829fdc737af
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-07 10:17:02 +0000

    done 4-cities_by_state

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+    This Script lists all states from the database `hbtn_0e_4_usa`
+    It takes username, password, database name
+    Makes use of MySQLdb.
+    Connects to MySQL server on localhost 3306
+    Result must be sorted in ascending order by `states.id`
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+
+    args = argv
+    db = MySQLdb.connect(host='localhost',
+                         user=args[1],
+                         passwd=args[2],
+                         db=args[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states s
+                JOIN cities c
+                ON s.id = c.state_id
+                ORDER BY c.id ASC;
+                """)
+    states = cur.fetchall()
+
+    for state in states:
+        print("({0}, '{1}', '{2}')".format(state[2], state[4], state[1]))
+
+    cur.close()
+    db.close()
