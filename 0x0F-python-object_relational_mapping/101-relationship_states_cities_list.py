8937e15120e8b77408eb359c5ebf27b218972a0d
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-09 21:59:16 +0000

    101-relationship_state fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,18 +12,16 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == '__main__':
-    if len(argv) >= 4:
-        db_string = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        engine = create_engine(db_string.format(
-            argv[1],
-            argv[2],
-            argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        session = sessionmaker(bind=engine)()
+    db_string = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(db_string.format(
+        argv[1],
+        argv[2],
+        argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
 
-        states = session.query(State).order_by(State.id)
-        for state in states:
-            print("{}: {}".format(state.id, state.name))
-            for city in state.cities:
-                print("    {}: {}".format(city.id, city.name))
-        session.close()
+    states = session.query(State).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("     {}: {}".format(city.id, city.name))
