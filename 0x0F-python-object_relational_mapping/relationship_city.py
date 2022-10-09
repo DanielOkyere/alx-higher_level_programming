e14867c0c9c455668c21c6585f52428d450c1a56
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-09 21:38:36 +0000

    fixed relationship city and state

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-
 """
-    This is the first model created defining state and
-    base instance.
+Contains the class definition of a City
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-from model_state import Base
+from relationship_state import Base
 
 
 class City(Base):
-    """
-        Representation of a row in cities table
-    """
-    __tablename__ = 'cities'
-    id = Column(Integer,
-                primary_key=True,
-                nullable=False,
-                autoincrement=True,
-                unique=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer,
-                      ForeignKey('states.id'),
-                      nullable=False
-                      )
+    """Represents a row in a cities table"""
+    __tablename__ = "cities"
+    id = Column(
+        Integer,
+        autoincrement=True,
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    name = Column(
+        String(length=128),
+        nullable=False
+    )
+    state_id = Column(
+        Integer,
+        ForeignKey('states.id'),
+        nullable=False
+    )
     state = relationship('State', back_populates='cities')
