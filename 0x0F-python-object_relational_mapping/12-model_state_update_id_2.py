91cc1626b75a1321991d86901da7d1b9b8cf1862
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-09 17:25:32 +0000

    fixies

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,10 +10,13 @@ from sys import argv
 
 from model_state import Base, State
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
+    db_string = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     if len(argv) >= 4:
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        engine = create_engine(db_string.format(
+            argv[1],
+            argv[2],
+            argv[3]), pool_pre_ping=True)
         Base.metadata.create_all(engine)
         session = sessionmaker(bind=engine)()
 
