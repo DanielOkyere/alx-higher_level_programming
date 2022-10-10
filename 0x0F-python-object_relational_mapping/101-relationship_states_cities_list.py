2e84c9b1c204b796167356549b2cc983480285f5
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-10 06:19:00 +0000

    101-relationship.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -18,7 +18,8 @@ if __name__ == '__main__':
         argv[2],
         argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     states = session.query(State).order_by(State.id)
     for state in states:
