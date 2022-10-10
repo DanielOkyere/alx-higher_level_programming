940e80d7d3ef97f2bf53b63fa56ec89542e11277
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-10 06:15:28 +0000

    fixes for 101-relationship_states

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -24,4 +24,4 @@ if __name__ == '__main__':
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("     {}: {}".format(city.id, city.name))
+            print("    {}: {}".format(city.id, city.name))
