4f337383bb9895c26557a3729415b4d29c0e9648
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-21 22:06:12 +0100

    fixed pycodestyle errors on 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     data = urllib.parse.urlencode({
-        'email':sys.argv[2]
+        'email': sys.argv[2]
     }).encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
