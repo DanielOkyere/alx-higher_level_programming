47a6ccab6c8d76ce47f5b8301b9da756e8bd1ad9
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-22 00:01:30 +0100

    Github authentications

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
 """
-
+script authenticates Github Api
 """
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    github_url = 'https://api.github.com/user'
+    params = {
+        'username': sys.argv[1],
+    }
+    h = {
+        'Authorization': 'Bearer ' + sys.argv[2]
+    }
+    r = requests.get(github_url, params=params, headers=h)
+    print(r.json().get('id'))
