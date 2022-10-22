0888b8f549905f312ce9b314a592ef951e223ca9
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-22 01:05:57 +0100

    hopefully done

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,15 +10,17 @@ if __name__ == '__main__':
     import sys
     import requests
 
-    url = 'https://api.github.com/repos/{}/{}/commits' \
-        .format(sys.argv[1], sys.argv[2])
-    params = {
-        'per_page': 10
-    }
-    r = requests.get(url, params=params)
-    res = r.json()
-    for el in res:
-        print('{}: {}'.format(
-            el.get('sha'),
-            el['commit']['author']['name']
-        ))
+    if len(sys.argv) > 2:
+        url = 'https://api.github.com/repos/{}/{}/commits' \
+            .format(sys.argv[1], sys.argv[2])
+        params = {
+            'per_page': 10
+        }
+        r = requests.get(url, params=params)
+        res = r.json()
+        if r.status_code == 200:
+            for el in res:
+                print('{}: {}'.format(
+                    el.get('sha'),
+                    el['commit']['author']['name']
+                ))
