1659b228e567c46c126d1bcc57de255b598eef4d
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-11-15 16:08:58 +0000

    Fixing error in 4-starwars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,7 +10,7 @@ request(process.argv[2], function (error, response, body) {
     const films = JSON.parse(body).results;
     let count = 0;
     for (const film in films) {
-      const characters = film[film].characters;
+      const characters = films[film].characters;
       for (const char in characters) {
         if (characters[char].includes('18')) {
           count++;
