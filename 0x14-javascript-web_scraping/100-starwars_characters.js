121cc172a1f2d3d6326257f81e679fdfd4848b8e
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-11-15 16:52:25 +0000

    100 - 101 fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,16 +6,16 @@
 
 const req = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/'
-req(url + id, function (error, response, body){
-    if (error) console.log(error);
-    else {
-        const character = JSON.parse(body).characters;
-        character.forEach((element) => {
-            req(element, function (err, res, content) {
-                if (error) console.log(err);
-                console.log(JSON.parse(content).name);
-            })
-        });
-    }
-})
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req(url + id, function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    const character = JSON.parse(body).characters;
+    character.forEach((element) => {
+      req(element, function (err, res, content) {
+        if (error) console.log(err);
+        console.log(JSON.parse(content).name);
+      });
+    });
+  }
+});
